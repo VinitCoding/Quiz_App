fa2fb93cb87ad118b02ef17ea2d486d8cf4332fa
VinitCoding <vinitcoding1001@gmail.com>
2024-11-06 12:03:50 +0530

    Quiz_UI_done_(036-11-2024)_at_(12:03)pm

--- frontend/src/pages/ExamPage.jsx
+++ frontend/src/pages/ExamPage.jsx
@@ -57,7 +57,92 @@ const ExamPage = () => {
 
       {/* Main Content */}
       <div>
-        Exam Page
+        {/* No. of Questions and timer section */}
+        <div className='flex justify-between pt-4 mx-6'>
+          <div className='bg-dark-blue text-white px-3 py-2.5 w-fit rounded-md font-medium'>
+            Questions 1 / 25
+          </div>
+
+          <div className='bg-dark-blue text-white px-3 py-2.5 w-fit rounded-md font-medium'>
+            Time Remaining - 00:00
+          </div>
+        </div>
+        
+        {/* Questions with options*/}
+        <div className='flex flex-col justify-center p-6 mx-auto mt-3 bg-white rounded-md shadow-md w-fit'>
+          <p className='text-lg font-semibold'>Q.1 Which data type is used to store a sequence of characters in Python?</p>
+          <ol className='flex flex-col mt-3 list-inside gap-y-5'>
+            <li className='px-2 py-1 border-[0.6px] border-black rounded-md text-lg cursor-pointer'>List</li>
+            <li className='px-2 py-1 border-[0.6px] border-black rounded-md text-lg'>Tuple</li>
+            <li className='px-2 py-1 border-[0.6px] border-black rounded-md text-lg'>Set</li>
+            <li className='px-2 py-1 border-[0.6px] border-black rounded-md text-lg'>String</li>
+          </ol>
+        </div>
+
+        {/* Previous and next button */}
+        <div className='flex justify-between mx-20 mt-8'>
+          <button className='bg-[#CAB123] px-4 py-1.5 font-medium text-white rounded-md'>Previous</button>
+          <button className='bg-[#3B9F3B] px-6 py-1.5 font-medium text-white rounded-md'>Next</button>
+        </div>
+
+        {/* Question numbers */}
+        <div className='grid grid-cols-3 px-6 py-4 bg-white mt-7 gap-y-4'>
+          {/* Category 1 */}
+          <div >
+            <h2 className='font-medium'>Cateogry 1</h2>
+            <ul className='flex mt-2 gap-x-6'>
+              <li className='border-[0.6px] border-black text-center px-3 py-0.5'>1</li>
+              <li className='border-[0.6px] border-black text-center px-3 py-0.5'>2</li>
+              <li className='border-[0.6px] border-black text-center px-3 py-0.5'>3</li>
+              <li className='border-[0.6px] border-black text-center px-3 py-0.5'>4</li>
+              <li className='border-[0.6px] border-black text-center px-3 py-0.5'>5</li>
+            </ul>
+          </div>
+          {/* Category 2 */}
+          <div >
+            <h2 className='font-medium'>Cateogry 2</h2>
+            <ul className='flex mt-2 gap-x-6'>
+              <li className='border-[0.6px] border-black text-center px-3 py-0.5'>6</li>
+              <li className='border-[0.6px] border-black text-center px-3 py-0.5'>7</li>
+              <li className='border-[0.6px] border-black text-center px-3 py-0.5'>8</li>
+              <li className='border-[0.6px] border-black text-center px-3 py-0.5'>9</li>
+              <li className='border-[0.6px] border-black text-center px-3 py-0.5'>10</li>
+            </ul>
+          </div>
+          {/* Category 3 */}
+          <div >
+            <h2 className='font-medium'>Cateogry 3</h2>
+            <ul className='flex mt-2 gap-x-6'>
+              <li className='border-[0.6px] border-black text-center px-3 py-0.5'>11</li>
+              <li className='border-[0.6px] border-black text-center px-3 py-0.5'>12</li>
+              <li className='border-[0.6px] border-black text-center px-3 py-0.5'>13</li>
+              <li className='border-[0.6px] border-black text-center px-3 py-0.5'>14</li>
+              <li className='border-[0.6px] border-black text-center px-3 py-0.5'>15</li>
+            </ul>
+          </div>
+          {/* Category 4 */}
+          <div >
+            <h2 className='font-medium'>Cateogry 4</h2>
+            <ul className='flex mt-2 gap-x-6'>
+              <li className='border-[0.6px] border-black text-center px-3 py-0.5'>16</li>
+              <li className='border-[0.6px] border-black text-center px-3 py-0.5'>17</li>
+              <li className='border-[0.6px] border-black text-center px-3 py-0.5'>18</li>
+              <li className='border-[0.6px] border-black text-center px-3 py-0.5'>19</li>
+              <li className='border-[0.6px] border-black text-center px-3 py-0.5'>20</li>
+            </ul>
+          </div>
+          {/* Category 5 */}
+          <div >
+            <h2 className='font-medium'>Cateogry 5</h2>
+            <ul className='flex mt-2 gap-x-6'>
+              <li className='border-[0.6px] border-black text-center px-3 py-0.5'>21</li>
+              <li className='border-[0.6px] border-black text-center px-3 py-0.5'>22</li>
+              <li className='border-[0.6px] border-black text-center px-3 py-0.5'>23</li>
+              <li className='border-[0.6px] border-black text-center px-3 py-0.5'>24</li>
+              <li className='border-[0.6px] border-black text-center px-3 py-0.5'>25</li>
+            </ul>
+          </div>
+        </div>
       </div>
     </section>
   )
